490ed581a33cae659d1fa658a9dd17c9c9745155
Nick Villone <n.villone8@gmail.com>
2022-06-21 21:58:46 -0400

    formSubmission works

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,22 +17,59 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-    if (testInput === "") {
-        return 'Empty'
-    }
 
-    if (isNaN(testInput) === true) {
+    if (testInput === "") {
+        console.log('Empty');
+        return "Empty";
+    
+    } else if (isNaN(testInput) === true) {
         return 'Not a number'
     } else {
         return 'Is a number'
     }
    
-}
+};
+
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+
     
+    let form = document.querySelector("form");
+
+    form.addEventListener("submit", function (event) {
+        pilot = document.querySelector("input[name=pilotName]").value;
+        copilot = document.querySelector("input[name=copilotName]").value;
+        fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        cargoLevel = document.querySelector("input[name=cargoMass").value;
+        list = document.getElementById("faultyItems");
+
+        let validatePilot = validateInput(pilot);
+        let validateCopilot = validateInput(copilot);
+        let validateCargoLevel = validateInput(cargoLevel);
+        let validateFuelLevel = validateInput(fuelLevel);
+
+    
+        if (validatePilot === 'Empty' || validateCopilot === 'Empty' || validateCargoLevel === 'Empty' || validateFuelLevel === 'Empty') {
+            alert('Form must be filled out')
+            return event.preventDefault()
+        };
+
+        if (validatePilot === 'Is a number' || validateCopilot === 'Is a number') {
+            alert('Pilot name or Copilot name cannot be a number')
+            return event.preventDefault()
+        }
+
+        if (validateCargoLevel === 'Not a number' || validateFuelLevel === 'Not a number') {
+            alert('Cargo Mass or Fuel Level must be a number')
+            return event.preventDefault()
+        }
+
+        list.children[0].innerHTML = `${pilot}`;
+
+    })
    
-}
+};
 
 async function myFetch() {
     let planetsReturned;
